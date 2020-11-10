2e5c6e883a931b0e70120347e40d8eb7799ef08c
Francesca <fracassani@gmail.com>
2020-11-10 12:33:38 +0100

    Start JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**VUE */
+const batman = new Vue({
+  el: "#batman",
+  data: {
+    todos: [
+      "Kill the Penguin",
+      "Kill Bane",
+      "Kill the Penguin",
+      "Seduce Catwoman"
+    ]
+  }
+});

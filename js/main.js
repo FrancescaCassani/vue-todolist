e2aaa14ecdc554994fa8e96aea0aef09e5b76d18
Francesca <fracassani@gmail.com>
2020-11-11 15:19:36 +0100

    Prove line through

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ const batman = new Vue({
   data: {
     newTodos: "",
     todos: [
-      {text: "Seduce Catwoman"},
-      {text: "Kill Bane"},
-      {text: "Wash the Batmobile"},
-      {text: "Kill the Penguin"},
-      {text: "Kill the Joker"}
+      {text: "Seduce Catwoman", status: null},
+      {text: "Kill Bane", status: null},
+      {text: "Wash the Batmobile", status: null},
+      {text: "Kill the Penguin", status: null},
+      {text: "Kill the Joker", status: null}
     ],
     imgLink: "./img/clipart18626.png"
   },
@@ -20,6 +20,9 @@ const batman = new Vue({
         });
         this.newTodos= '' //per lasciare lo spazio vuoto appena aggiungi un todo
       }
+    },
+    checked(i) {
+      this.todos[i].status = !this.todos[i].status;
     },
     remove(i) {
       this.todos.splice(i, 1) //numero di elementi che voglio eliminare
